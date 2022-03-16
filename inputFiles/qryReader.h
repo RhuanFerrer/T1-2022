b07f2261e2b352ef0f0fed7a6b5bb885294ab53b
Rhuan <79750187+RhuanFerrer@users.noreply.github.com>
2022-03-16 15:32:07 -0300

    First Commit

---
+++ inputFiles/qryReader.h
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#pragma once
+
+class qryReader
+{
+private:
+    std::ifstream *file;
+
+public:
+    qryReader(std::string fileName);
+
+    // Read method
+
+    // Execute method
+
+    ~qryReader();
+};
---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "./inputFiles/qryReader.h"
+
+int main(int argc, char *argv[])
+{
+
+    // Read GEO
+
+    // Print first SVG
+
+    // Read QRY
+
+    // Execute QRY
+
+    // Print second SVG
+
+    return 0;
+}
