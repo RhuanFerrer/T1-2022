3c78b044971684afb31151eeea2c8ca880e45b9b
Rhuan <79750187+RhuanFerrer@users.noreply.github.com>
2022-03-20 00:46:51 -0300

    store terminal commands

--- gstorage.cpp
+++ gstorage.cpp
@@ -1,8 +1,54 @@
 #include "gstorage.h"
 
-storage *storage::instance = nullptr;
+#include <iostream>
 
-storage* storage::getInstance() {
-    static storage instance;
-    return &instance;
+gstorage *gstorage::instance = nullptr;
+
+gstorage::gstorage() {}
+
+gstorage *gstorage::getInstance() {
+    if (instance == nullptr) 
+        instance = new gstorage();
+    
+    return instance;
+}
+
+void gstorage::setGeoFileName(std::string geoFileName) {
+    this->geoFileName = geoFileName;
+}
+
+void gstorage::setQryFileName(std::string qryFileName) {
+    this->qryFileName = qryFileName;
+}
+
+void gstorage::setInputPath(std::string inputPath) {
+    this->inputPath = inputPath;
+}
+
+void gstorage::setOutputPath(std::string outputPath) {
+    this->outputPath = outputPath;
+}
+
+std::string gstorage::getGeoFileName() {
+    return this->geoFileName;
+}
+
+std::string gstorage::getQryFileName() {
+    return this->qryFileName;
+}
+
+std::string gstorage::getInputPath() {
+    return this->inputPath;
+}
+
+std::string gstorage::getOutputPath() {
+    return this->outputPath;
+}
+
+std::ostream& operator<<(std::ostream& os, const gstorage& gs){
+    os << "geoFileName: " << gs.geoFileName << std::endl;
+    os << "qryFileName: " << gs.qryFileName << std::endl;
+    os << "inputPath: " << gs.inputPath << std::endl;
+    os << "outputPath: " << gs.outputPath << std::endl;
+    return os;
 }
--- gstorage.h
+++ gstorage.h
@@ -1,13 +1,34 @@
 /*    Includes    */
+#include <iostream>
 
 #pragma once
 
-class storage {
+class gstorage {
    private:
-    storage();
-    ~storage();
-    static storage *instance;
+    gstorage();
+
+    static gstorage *instance;
+
+    /*      ~ARGV~      */
+    std::string geoFileName;
+    std::string qryFileName;
+    std::string inputPath;
+    std::string outputPath;
 
    public:
-    static storage* getInstance();
+    
+    static gstorage* getInstance();
+    
+    /*      ~ARGV~      */
+    void setGeoFileName(std::string geoFileName);
+    void setQryFileName(std::string qryFileName);
+    void setInputPath(std::string inputPath);
+    void setOutputPath(std::string outputPath);
+    std::string getGeoFileName();
+    std::string getQryFileName();
+    std::string getInputPath();
+    std::string getOutputPath();
+
+    friend std::ostream& operator<<(std::ostream& os, const gstorage& gs);
+
 };
