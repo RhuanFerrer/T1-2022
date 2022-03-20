bb8daa6f9d8843296b0bd18bd893920547d48bac
felipe watanaBASED <42080804+felipeyousoro@users.noreply.github.com>
2022-03-20 11:32:43 -0300

    iniciando arquivos de escrita no svg

---
+++ svgWrite.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include "svgWrite.hpp"
+
+void svgWrite::startSVG(std::string filePath){
+    //std::remove(filePath); 
+    
+    std::ofstream svgFile;
+    svgFile.open(filePath);
+
+    svgFile << "<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">" << std::endl;
+
+    svgFile.close();
+};
